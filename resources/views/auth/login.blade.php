2f2052a0dd84d2a23a5400bfc0b0ea041205d9d4
basyri <bashussain04@gmail.com>
2020-03-10 14:27:04 +0700

    update-login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,147 @@
+ 
+<!--
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{-- {{ __('Login') }} --}}
+                                </button>
+
+                                {{-- @if (Route::has('password.request')) --}}
+                                    {{-- <a class="btn btn-link" href="{{ route('password.request') }}"> --}}
+                                        {{-- {{ __('Forgot Your Password?') }} --}}
+                                    </a>
+                                {{-- @endif --}}
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+-->
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inventory Stock Management | Login</title>
+    <link rel="stylesheet" href="{{asset('admin/assets/vendor/simple-line-icons/css/simple-line-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('admin/assets/vendor/font-awesome/css/fontawesome-all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('admin/assets/css/styles.css')}}">
+</head>
+<body>
+<div class="page-wrapper flex-row align-items-center">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <div class="card p-4">
+                    <div class="card-header text-center text-uppercase h5 font-weight">
+                        Inventory Stock Management
+                    </div>
+                    <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                        <div class="card-body py-2">
+                            <div class="form-group">
+                                <!-- <label class="form-control-label">Email</label> -->
+                                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder='Email' autofocus >
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <!-- <label class="form-control-label">Password</label> -->
+                                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder='Password'>
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+
+                            <div class="custom-control custom-checkbox mt-4">
+                                <input type="checkbox" class="custom-control-input" id="login" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <label class="custom-control-label" for="login">Remember me</label>
+                            </div>
+                        </div>
+                    
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class="col-6">
+                                    <button type="submit" class="btn btn-primary px-5">Login</button>
+                                </div>
+
+                                <div class="col-6">
+                                    <a href="{{ route('password.request') }}" class="btn btn-link">Forgot password?</a>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="{{asset('admin/assets/vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('admin/assets/vendor/popper.js/popper.min.js')}}"></script>
+<script src="{{asset('admin/assets/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('admin/assets/vendor/chart.js/chart.min.js')}}"></script>
+<script src="{{asset('admin/assets/js/carbon.js')}}"></script>
+<script src="{{asset('admin/assets/js/demo.js')}}"></script>
+</body>
+</html>
+
